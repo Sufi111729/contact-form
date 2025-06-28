a94ea7c1bd838be2b073765c6c9e8921c513d373
Sufi111729 <sufi111729@gmail.com>
2025-06-28 20:33:56 +0530

    Fix: Add @Entity and package scan for Contact

--- src/main/java/com/contect/sufi/ContectBackEndApplication.java
+++ src/main/java/com/contect/sufi/ContectBackEndApplication.java
@@ -2,8 +2,14 @@ package com.contect.sufi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan("com.contect.sufi.entity") // 👈 Add this
+@EnableJpaRepositories("com.contect.sufi.repository")
+
 public class ContectBackEndApplication {
 
 	public static void main(String[] args) {
